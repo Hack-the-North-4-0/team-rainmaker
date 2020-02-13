4d7ca62b0d1d7d5df5474d2cfd3b00add6d4c3e4
Steve Elliott <steve@tegud.net>
2020-02-13 17:50:01 +0000

    more functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,24 +3,69 @@ import './App.css';
 
 import Home from './components/home';
 import GameView from './components/game-view';
+import Completed from './components/completed';
 
+const questions = [
+  {
+    question: 'question one text',
+    answers: [
+      { answer: 'Answer 1', correct: false },
+      { answer: 'Answer 2', correct: true },
+      { answer: 'Answer 3', correct: false },
+    ],
+  },
+];
+
+const generateNewGame = () => {
+  const remainingQuestions = [...questions];
+
+  return { round: 1, remainingQuestions, currentQuestion: remainingQuestions.shift() };
+};
 
 function App() {
   const [gameState, dispatch] = useReducer((state, { event, ...rest }) => {
     if (event === 'game-start') {
       const { name } = rest;
 
-      return { name, round: 1 };
+      return { name, ...generateNewGame() };
     }
 
-    return {};
+    if (event === 'answer-question') {
+      const [currentQuestion, ...remainingQuestions] = state.remainingQuestions;
+
+      if (!currentQuestion) {
+        return { ...state, complete: true };
+      }
+
+      return { ...state, round: state.round + 1, remainingQuestions, currentQuestion };
+    }
+
+    if (event === 'reset') {
+      return { name: state.name, ...generateNewGame() };
+    }
+
+    return state;
   }, {});
+
+  if (!gameState.name) {
+    return (
+      <div className="App">
+        <Home dispatch={dispatch}></Home>
+      </div>
+    );
+  }
+
+  if (gameState.completed) {
+    return (
+      <div className="App">
+        <Completed gameState={gameState} />
+      </div>
+    );
+  }
 
   return (
     <div className="App">
-      {!gameState.name
-        ? <Home dispatch={dispatch}></Home>
-        : <GameView dispatch={dispatch} gameState={gameState}></GameView>}
+      <GameView dispatch={dispatch} gameState={gameState}></GameView>
     </div>
   );
 }
--- frontend/src/components/game-view/index.js
+++ frontend/src/components/game-view/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 
-export default ({ gameState }) => {
-  return <div>
-    <h2>Game Round {gameState.round}</h2>
+import './game.css';
 
+export default ({ dispatch, gameState }) => {
+  const { currentQuestion, round } = gameState;
+
+  const answerQuestion = () => {
+    dispatch({ event: 'answer-question' });
+  };
+
+  return <div>
+    <h2>Game Round {round}</h2>
+
+    <p>
+      {currentQuestion.text}
+    </p>
+
+    <ul className="game__question-answers">
+      {currentQuestion.answers.map(({ answer }, i) => {
+        return <li key={`answer-${gameState.round}-${i}`}><label><input type="radio" name="question-answer" value={`answer-${i + 1}`} /> {answer}</label></li>;
+      })}
+    </ul>
+
+    <button onClick={() => answerQuestion()}>Answer</button>
   </div>;
 };
