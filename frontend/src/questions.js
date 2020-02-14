351abb6ff6310e9750c0a4eaf3f7c21c3a7a9f87
red-chris <61047411+red-chris@users.noreply.github.com>
2020-02-14 10:57:23 +0000

    Update questions.js

--- frontend/src/questions.js
+++ frontend/src/questions.js
@@ -4,8 +4,8 @@ const allQuestions = [
     answers: [
       { answer: 'Takeaway', correct: false },
       { answer: 'New Phone', correct: false },
-      { answer: 'Trainers', correct: false },
-      { answer: 'Cap', correct: true },
+      { answer: 'New Nike Airs', correct: false },
+      { answer: 'Second hand moped', correct: true },
     ],
   },
   {
@@ -14,22 +14,24 @@ const allQuestions = [
       { answer: 'Phone Contract', correct: true },
       { answer: 'New Trainers', correct: false },
       { answer: 'Netflix Subscription', correct: false },
+      { answer: 'A loyalty card', correct: false },
     ],
   },
   {
     question: 'What is a loan',
     answers: [
       { answer: 'Free Money', correct: false },
-      { answer: 'Money you owe someone', correct: true },
+      { answer: 'Money you owe somebody', correct: true },
       { answer: 'What you get paid', correct: false },
+      { answer: 'Someone in a room by themself', correct: false },
     ],
   },
   {
-    question: 'You are given £20 for your birthday, whats the best way to turn this into £30',
+    question: 'You are given £20 for your birthday, what\'s the best way to turn this into £30',
     answers: [
       { answer: 'Stick it under a mattress', correct: true },
       { answer: 'Spend it', correct: false },
-      { answer: 'Savings', correct: false },
+      { answer: 'Save it with a bank', correct: false },
       { answer: 'Lottery tickets', correct: false },
     ],
   },
@@ -41,12 +43,12 @@ const allQuestions = [
       { answer: 'Only when I own my own home', correct: false },
       { answer: 'When my bin is full and needs taking away', correct: false },
     ],
-  }
+  },
  {
     question: 'What happens when you spend more than you earn as income',
     answers: [
       { answer: 'You pay less tax', correct: false },
-      { answer: 'Getting into debt', correct: true },
+      { answer: 'You get into debt', correct: true },
       { answer: 'It means you are budgeting well', correct: false },
       { answer: 'Nothing happens', correct: false },
     ],
@@ -65,7 +67,7 @@ const allQuestions = [
     answers: [
       { answer: 'Money you can throw away', correct: true },
       { answer: 'Money you have to spend fast', correct: false },
-      { answer: 'Money leftover after you\'ve bought essentials', correct: true },
+      { answer: 'Money left after you\'ve bought essentials', correct: true },
       { answer: 'Money you spend on rent', correct: false },
     ],
   },
@@ -73,7 +75,7 @@ const allQuestions = [
     question: 'Which of these is the worst way to borrow £20',
     answers: [
       { answer: 'A payday loan', correct: true },
-      { answer: 'Using a free overdraft', correct: false },
+      { answer: 'A free overdraft', correct: false },
       { answer: 'A credit card', correct: false },
       { answer: 'Ask your mate', correct: false },
     ],
@@ -84,7 +86,7 @@ const allQuestions = [
       { answer: 'A lolly ice', correct: false },
       { answer: 'An income spending app', correct: false },
       { answer: 'A KPOP band', correct: false },
-      { answer: 'An individual savings accounts', correct: true },
+      { answer: 'An individual savings account', correct: true },
     ],
   },
 ];
