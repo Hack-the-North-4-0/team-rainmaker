994f0521669e4de0fdd8bf4ea0ec9c4dcab3ebcd
Steve Elliott <steve@tegud.net>
2020-02-14 13:12:27 +0000

    icons for completed

--- frontend/src/components/completed/index.js
+++ frontend/src/components/completed/index.js
@@ -3,12 +3,21 @@ import React, { useState } from 'react';
 import BankSummary from '../bank-summary';
 
 import './completed.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGrinStars, faLaughSquint, faSadCry } from '@fortawesome/free-solid-svg-icons';
 
 const endMessage = (ledger) => {
   const total = ledger.reduce((total, { amount = 0 }) => total + amount, 0);
 
   if (total > 300) {
-    return 'Wow; you’re a real superstar! You really showed off your finance knowledge! ';
+    return <div className="results-message">
+      <div className="results-message__icon">
+        <FontAwesomeIcon icon={faGrinStars} />
+      </div>
+      <div className="results-message__text">
+        Wow; you’re a real superstar! You really showed off your finance knowledge!
+      </div>
+    </div>;
   }
 
   // if (total > 250) {
@@ -20,17 +29,29 @@ const endMessage = (ledger) => {
   // }
 
   if (total >= 0) {
-    return 'Great effort - you got things right but can be faster next time! Well done!';
+    return <div className="results-message">
+      <div className="results-message__icon">
+        <FontAwesomeIcon icon={faLaughSquint} />
+      </div>
+      <div className="results-message__text">
+        Great effort - you got things right but can be faster next time! Well done!
+      </div>
+    </div>;
   }
 
-  return 'Oof! You’re in debt! Take some time to learn about money and have another go at getting back to in credit! ';
+  return <div className="results-message">
+    <div className="results-message__icon">
+      <FontAwesomeIcon icon={faSadCry} />
+    </div>
+    <div className="results-message__text">
+      Oof! You’re in debt! Take some time to learn about money and have another go at getting back to in credit!
+    </div>
+  </div>;
 };
 
 const ResultPanel = ({ gameState: { ledger, answers } }) => {
   return <div>
-    <div className="results-message">
-      {endMessage(ledger)}
-    </div>
+    {endMessage(ledger)}
     <BankSummary ledger={ledger} showAll={true} />
   </div>;
 };
